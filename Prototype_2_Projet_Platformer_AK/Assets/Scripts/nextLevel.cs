5e9be342b8b955ea298d39a7cdb06cb5c69db9f0
Alan Kilby <alan.kilby.cerquera@gmail.com>
2020-05-26 15:17:37 +0200

    Implemented Portal + Animation Fix
    Now the tutorial ending picture makes you go through a portal to the lvl 1. 
    
    Now the player has animations in the lvl 1.

---
+++ Prototype_2_Projet_Platformer_AK/Assets/Scripts/nextLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextLevel : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.gameObject.CompareTag("Player"))
+        {
+            Invoke("NextLevelTrigger", 1);
+            Debug.Log("Player Triggered");
+        }
+    }
+
+
+    public void NextLevelTrigger()
+    {
+        SceneManager.LoadScene("CM_Cimetière+ville");
+
+    }
+}

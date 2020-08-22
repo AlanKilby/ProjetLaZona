7aeb04f9f307daa7b68000b67c899c7fe89f51b4
Alan Kilby <alan.kilby.cerquera@gmail.com>
2020-08-22 18:34:59 +0200

    MenuMusic

---
+++ Prototype_2_Projet_Platformer_AK/Assets/Scripts/MenuMusic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.LowLevel;
+
+public class MenuMusic : MonoBehaviour
+{
+
+    public AudioSource titleIntro;
+    public AudioSource titleLoop;
+    public float lenghtHolder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lenghtHolder = titleIntro.clip.length;
+        Invoke("playIntro", 5);
+        Invoke("playLoop", (lenghtHolder+5));
+    }
+
+    public void playLoop()
+    {
+        titleLoop.Play();
+    }
+
+    public void playIntro()
+    {
+        titleIntro.Play();
+    }
+}

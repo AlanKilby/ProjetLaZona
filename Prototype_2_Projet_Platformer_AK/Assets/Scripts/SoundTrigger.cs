3324420cf28f5338aa453adadc24ced0d54688f7
Alan Kilby <alan.kilby.cerquera@gmail.com>
2020-08-23 07:52:01 +0200

    Tweak Patch 1
    Audio Trigger - Waking Anim - Final Score

---
+++ Prototype_2_Projet_Platformer_AK/Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTrigger : MonoBehaviour
+{
+    public AudioSource altarSound;
+
+    private void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.gameObject.CompareTag("Player"))
+        {
+            altarSound.Play();
+        }
+    }
+}

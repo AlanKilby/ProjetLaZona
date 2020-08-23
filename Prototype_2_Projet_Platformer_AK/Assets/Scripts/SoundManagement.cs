76028adc2ac683e51dc1844bea3f1bdb18cd50e0
Alan Kilby <alan.kilby.cerquera@gmail.com>
2020-08-23 18:50:49 +0200

    Pinata Candy Fix

--- Prototype_2_Projet_Platformer_AK/Assets/Scripts/SoundManagement.cs
+++ Prototype_2_Projet_Platformer_AK/Assets/Scripts/SoundManagement.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SoundManagement : MonoBehaviour
 {
+    public AudioMixerGroup master;
     public AudioSource jump;
     public AudioSource dash;
     public AudioSource death;
     public AudioSource candy;
     public AudioSource pinata;
 
+    private void Start()
+    {
+        jump.outputAudioMixerGroup = master;
+        dash.outputAudioMixerGroup = master;
+        death.outputAudioMixerGroup = master;
+        candy.outputAudioMixerGroup = master;
+        pinata.outputAudioMixerGroup = master;
+
+    }
 }
